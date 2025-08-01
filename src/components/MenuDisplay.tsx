fac727c561c6a3fcddc01da44d2f87e8e016b8c8
[dyad] <abaleemmohammed@gmail.com>
2025-08-01 12:11:58 -0500

    [dyad] Recreated second screen layout to fix missing and cut-off sections - wrote 1 file(s)

--- src/components/MenuDisplay.tsx
+++ src/components/MenuDisplay.tsx
@@ -114,10 +114,10 @@ const MenuDisplay = () => {
         <div className="w-1/2 flex-shrink-0 p-4 md:p-8">
           <div className="flex flex-row gap-x-8 mx-auto w-full h-full">
 
-            {/* Left Column of Second Half */}
+            {/* Left Column of Second Half (Appetizers, Salads) */}
             <div className="flex flex-col flex-1 gap-y-8">
               {/* Appetizers Section */}
-              <div className="bg-red-900 p-6 rounded-lg shadow-lg flex-1 flex flex-col">
+              <div className="bg-red-900 p-6 rounded-lg shadow-lg flex flex-col">
                 <h2 className="text-5xl font-extrabold text-yellow-400 mb-4 border-b-4 border-yellow-400 pb-4 uppercase">Appetizers</h2>
                 <div className="flex flex-col flex-grow justify-around">
                   <MenuItem
@@ -140,7 +140,7 @@ const MenuDisplay = () => {
               </div>
 
               {/* Salads Section */}
-              <div className="bg-red-900 p-6 rounded-lg shadow-lg flex-1 flex flex-col">
+              <div className="bg-red-900 p-6 rounded-lg shadow-lg flex flex-col">
                 <h2 className="text-5xl font-extrabold text-yellow-400 mb-4 border-b-4 border-yellow-400 pb-4 uppercase">Salads</h2>
                 <p className="text-lg sm:text-xl text-gray-100 mb-2">Add Chicken to any salad for additional $4.50 or Gyro meat for additional $4.99</p>
                 <div className="flex flex-col flex-grow justify-around">
@@ -160,7 +160,7 @@ const MenuDisplay = () => {
               </div>
             </div>
 
-            {/* Right Column of Second Half */}
+            {/* Right Column of Second Half (Sides, Soup, Kids, Desserts) */}
             <div className="flex flex-col flex-1 gap-y-8">
               {/* Sides Section */}
               <div className="bg-red-900 p-6 rounded-lg shadow-lg flex-1 flex flex-col">
@@ -177,10 +177,10 @@ const MenuDisplay = () => {
                 </div>
               </div>
 
-              {/* Combined Column for Soup, Kids, Desserts */}
-              <div className="flex flex-col gap-y-8 flex-1">
+              {/* Combined Container for Soup, Kids, Desserts - allows them to stack naturally */}
+              <div className="flex flex-col gap-y-8">
                 {/* Soup Section */}
-                <div className="bg-red-900 p-6 rounded-lg shadow-lg flex flex-col"> {/* Removed flex-1 */}
+                <div className="bg-red-900 p-6 rounded-lg shadow-lg flex flex-col">
                   <h2 className="text-5xl font-extrabold text-yellow-400 mb-4 border-b-4 border-yellow-400 pb-4 uppercase">Soup</h2>
                   <div className="flex flex-col flex-grow justify-around">
                     <MenuItem name="Lentil Soup" price="$4.99" />
@@ -188,7 +188,7 @@ const MenuDisplay = () => {
                 </div>
 
                 {/* Kids Meals Section */}
-                <div className="bg-red-900 p-6 rounded-lg shadow-lg flex flex-col"> {/* Removed flex-1 */}
+                <div className="bg-red-900 p-6 rounded-lg shadow-lg flex flex-col">
                   <h2 className="text-5xl font-extrabold text-yellow-400 mb-4 border-b-4 border-yellow-400 pb-4 uppercase">Kids Meals</h2>
                   <p className="text-lg sm:text-xl text-gray-100 mb-2">Served with choice of chips or fries and a drink</p>
                   <div className="flex flex-col flex-grow justify-around">
@@ -197,7 +197,7 @@ const MenuDisplay = () => {
                 </div>
 
                 {/* Desserts Section */}
-                <div className="bg-red-900 p-6 rounded-lg shadow-lg flex flex-col"> {/* Removed flex-1 */}
+                <div className="bg-red-900 p-6 rounded-lg shadow-lg flex flex-col">
                   <h2 className="text-5xl font-extrabold text-yellow-400 mb-4 border-b-4 border-yellow-400 pb-4 uppercase">Desserts</h2>
                   <div className="flex flex-col flex-grow justify-around">
                     <MenuItem name="Baklava" price="$3.50" />
