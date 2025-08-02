8d7460700ce57e039827f8d485eb07648fc8ea06
[dyad] <abaleemmohammed@gmail.com>
2025-08-01 20:59:16 -0500

    [dyad] Moved the logo to the top right corner. - wrote 1 file(s)

--- src/components/MenuDisplay.tsx
+++ src/components/MenuDisplay.tsx
@@ -21,11 +21,11 @@ const MenuDisplay = () => {
 
   return (
     <div className="relative w-screen h-screen overflow-hidden bg-gray-950 text-gray-200 font-bold">
-      {/* Logo positioned at the top center */}
+      {/* Logo positioned at the top right corner */}
       <img 
         src="/manna-grill-logo.png" 
         alt="Manna Mediterranean Grill Logo" 
-        className="absolute top-4 left-1/2 transform -translate-x-1/2 z-10 w-32 md:w-48 lg:w-64" 
+        className="absolute top-4 right-4 z-10 w-32 md:w-48 lg:w-64" 
       />
 
       <div
